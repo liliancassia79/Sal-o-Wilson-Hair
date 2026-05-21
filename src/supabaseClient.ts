7768f28c2cda6dbc76a8afa1d7ec615da8fc8d21
Lilian Sousa <liliancassia79@gmail.com>
2026-05-21 16:07:16 -0300

    Adiciona fallback e limpeza nas chaves do Supabase

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL?.replace(/"/g, '') || "https://vujamdesjosylizgntvw.supabase.co";
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY?.replace(/"/g, '') || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ1amFtZGVzam9zeWxpemdudHZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkyOTAwOTIsImV4cCI6MjA5NDg2NjA5Mn0.KLIPelghdMNt9AcytYrKdSXqe-BdRJyGgKVRQoYjNkY";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
